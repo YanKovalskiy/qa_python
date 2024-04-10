6cb0357d44da6ab0546374a12c99d87307d7a876
Vladimir Yankovskiy <vyankovskiy@gmail.com>
2024-04-10 20:28:26 +0500

    Перенес фикстуры в conftest.py

--- test_main.py
+++ test_main.py
@@ -4,23 +4,6 @@ import pytest
 
 class TestBooksCollector:
 
-    @pytest.fixture()
-    def collector(self):
-        return BooksCollector()
-
-    @pytest.fixture()
-    def date_books(self, collector):
-        book = {
-            'Рассказы о Шерлоке Холмсе': 'Детективы',
-            'Двадцать тысяч лье под водой': 'Фантастика',
-            'Человек-амфибия': 'Фантастика',
-            'Хребты безумия': 'Ужасы',
-        }
-
-        for book_name, book_genre in book.items():
-            collector.add_new_book(book_name)
-            collector.set_book_genre(book_name, book_genre)
-
     def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
