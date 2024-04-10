fd18147787f4fc28c5ae8e241e983bc11e0d8ec4
Vladimir Yankovskiy <vyankovskiy@gmail.com>
2024-04-10 11:40:35 +0500

    Coverage 2/10 methods

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# qa_python
-# Владимир Янковский
+### Владимир Янковский, qa_python, 8 когорта
+#### финальный проект 4 спринта автоматизации
+---
+- **test_add_new_book_add_two_identical_books** - проверка добавление двух одинаковых книг
+- **test_add_new_book_incorrect_length_book_name** - проверка добавления книг с пустым пустым названием и названием с длинной более 40 символов
+- **test_set_book_genre_add_existing_genre_to_existing_book** - проверка добавления существующего жанра к существующей книге
+- **test_set_book_genre_add_genre_to_existing_book** - проверка добавления жанра к существующей книге
+- **test_set_book_genre_add_genre_to_not_existing_book** - проверка добавления жанра к не существующей книге
--- tests.py
+++ tests.py
@@ -1,13 +1,9 @@
 from main import BooksCollector
+import pytest
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
@@ -16,9 +12,49 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_two_identical_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение')
+        collector.add_new_book('Гордость и предубеждение')
+
+        assert len(collector.get_books_genre()) == 1
+
+    @pytest.mark.parametrize(
+        'book_name',
+        ('',
+         'Круглосуточный книжный мистера Пенумбры!!')  # 41 символ
+    )
+    def test_add_new_book_incorrect_length_book_name(self, book_name):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+
+        assert len(collector.get_books_genre()) == 0
+
+    @pytest.mark.parametrize(
+        'book_name, book_genre, expected_result',
+        (
+                ('Двадцать тысяч лье под водой', 'Фантастика', 'Фантастика'),
+                ('Двадцать тысяч лье под водой', 'Мелодрама', ''),
+        )
+    )
+    def test_set_book_genre_add_genre_to_existing_book(self, book_name, book_genre, expected_result):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, book_genre)
+
+        assert collector.get_book_genre(book_name) == expected_result
+
+    @pytest.mark.parametrize(
+        'book_genre',
+        (
+                'Фантастика', 'Мелодрама'
+        )
+    )
+    def test_set_book_genre_add_genre_to_not_existing_book(self, book_genre):
+        collector = BooksCollector()
+        book_name = 'Двадцать тысяч лье под водой'
+        collector.set_book_genre(book_name, book_genre)
+
+        assert collector.get_book_genre(book_name) is None
